5e76ed5ebd0924c3e5d7919effdaf53a4d62b4e0
Abhayprad Jha <abhaypradjha@gmail.com>
2018-11-29 19:42:47 -0500

    Quick commit for saving progress. Do not revert to here. Created Elevator

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
@@ -0,0 +1,16 @@
+import org.firstinspires.ftc.teamcode.hardware;
+import org.org.firstinspires.ftc.teamcode.controllers;
+
+public class Elevator {
+  LinearActuator act1;
+  LinearActuator act2;
+  @Override
+  public void init() {
+    act1 = new LinearActuator();
+    act2 = new LinearActuator();
+
+  }
+  @Override
+  public void update() {
+  }
+}
