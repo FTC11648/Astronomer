93849a7dd162bc6b160804790b25b1281448b9d4
Quunii <kennethmiuraubw@gmail.com>
2018-12-01 21:56:58 -0500

    updated Elevator system

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Elevator.java
@@ -1,15 +1,17 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import org.firstinspires.ftc.teamcode.hardware.LinearActuator;
+import org.firstinspires.ftc.teamcode.hardware.controls.GamepadWrapper;
+
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Elevator implements Subsystem{
   private LinearActuator actuator1;
   private LinearActuator actuator2;
-  private Gamepad manipController;
+  private GamepadWrapper manipController;
 
-  public Elevator(Gamepad manipController, LinearActuator actuator1, LinearActuator actuator2) {
+  public Elevator(GamepadWrapper manipController, LinearActuator actuator1, LinearActuator actuator2) {
     this.manipController = manipController;
     this.actuator1 = actuator1;
     this.actuator2 = actuator2;
@@ -21,7 +23,7 @@ public class Elevator implements Subsystem{
   }
   @Override
   public void update() {
-    double leftStickY = -manipController.left_stick_y;
+    double leftStickY = manipController.getLeftStickY();
     actuator1.setVolt(leftStickY);
     actuator2.setVolt(leftStickY);
   }
