669f84975ab700d801b1748edbbfcce85146fd6a
juice <jbak@mtech.edu>
2019-11-23 14:45:24 -0700

    tried fixing grabber

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Grabber.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.subsystems.subsystemutils.Subsystem;
@@ -19,19 +20,24 @@ public class Grabber implements Subsystem {
     }
 
     @Override
-    public void init() {}
+    public void init() {
+        rotate.setPower(0);
+    }
 
     @Override
     public void update() {
         if (manipController.a) {
-            latch.setPower(1);
+            latch.setPower(0);
         } else if (manipController.b) {
-            latch.setPower(-1);
+            latch.setPower(0);
         } else {
             latch.setPower(0);
         }
-        if (manipController.left_stick_y > .2 || manipController.left_stick_y < -.2) {
-            rotate.setPower(manipController.left_stick_y);
+
+        if (manipController.left_stick_y > .2 ) {
+            rotate.setPower(0.7);
+        } else if (manipController.left_stick_y < -.2) {
+            rotate.setPower(0.3);
         } else {
             rotate.setPower(0);
         }
